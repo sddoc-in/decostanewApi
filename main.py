e816c6694bb2c5aaf2591626c9dbd0894a8dbba9
Deepak <rohit45deepak@gmail.com>
2024-06-02 22:19:19 +0530

    done

--- main.py
+++ main.py
@@ -418,4 +418,4 @@ async def getdata(background_tasks: BackgroundTasks, SearchID : str = Query(None
     
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=80)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
